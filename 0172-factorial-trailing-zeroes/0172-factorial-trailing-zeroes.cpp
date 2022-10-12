08c65e0bd2df7c4075a23f6dab5d7651ccd570dc
snandkule <snandkule@gmail.com>
2022-10-12 16:56:10 +0530

    Time: 4 ms (40.59%), Space: 5.9 MB (29.34%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,31 +1,13 @@
 class Solution {
 public:
-    int trailingZeroes(int n) {
+    int trailingZeroes(int A) {
         
-        long long int twos=0,fives=0;
-        
-        while(n>1){
-            
-            int t = n;
-            while(t>1){
-                if(t%2==0){
-                    twos++;
-                    t=t/2;
-                }else{
-                    break;
-                }
-            }
-            while(t>1){
-                if(t%5==0){
-                    fives++;
-                    t=t/5;
-                }else{
-                    break;
-                }
-            }
-            n--;
+       int sum = 0;
+        while (A/ 5 > 0) {
+            sum += (A/ 5);
+            A/= 5;
         }
-        return min(fives,twos);
+        return sum;
         
     }
 };
