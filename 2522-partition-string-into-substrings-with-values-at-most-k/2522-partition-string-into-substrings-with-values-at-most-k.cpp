a029e3a0d787d840b88889d58ccba21c08860321
snandkule <snandkule@gmail.com>
2023-01-24 00:50:02 +0530

    Time: 1180 ms (5.01%), Space: 73.5 MB (5.17%) - LeetHub

---
+++ 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    vector<long long int> arr;
+    
+    long long int getans(string& s, int k, int pos){
+        
+        if(arr[pos]!=s.length()+1)return arr[pos];
+        if(pos==s.length()-1){
+            if(s[pos]-'0'<=k)
+                return 1;
+            return -1;
+        }
+        if(pos>=s.length())return 0;
+        long long int tmp=INT_MAX;
+        if(to_string(k).length()>=s.length()-pos){
+             long int sub1 = stol(s.substr(pos));
+            if(sub1<=k){
+                tmp=1;
+            }
+        }
+        for(int i=1; i<=to_string(k).length()&& pos+i<s.length();i++)
+        {
+            //take substr pos to i and i to remaining
+            long int sub1 = stol(s.substr(pos,i));
+            if(sub1<=k){
+                long long int nextans = getans(s,k,pos+i);
+                if(nextans!=-1)
+                    tmp = min(tmp, 1+nextans);
+            }
+            //check if minimum then it is the ans//assign in arr and return;
+        }
+        if(tmp==INT_MAX)tmp= -1;
+        if(arr[pos]==s.length()+1){
+            arr[pos] = tmp;
+        }
+        
+        return tmp;
+    }
+    
+    int minimumPartition(string s, int k) {
+    arr.assign(s.length(),s.length()+1);
+        
+        return getans(s,k,0);
+        
+    }
+};
