fab6e006b4814146ca1527a56293b8326dda86cd
snandkule <snandkule@gmail.com>
2023-04-14 23:46:19 +0530

    Time: 14 ms (62.12%), Space: 16 MB (61.18%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& A) {
+        map<int,int> m1;
+        vector<int> ans;
+    for(int i=0;i<A.size();i++){
+        if(m1.find(A[i])==m1.end()){
+            m1[A[i]]=1;
+        }else{
+            m1[A[i]]++;
+        }
+        // cout<<"ceil is "<<ceil((double)A.size()/2)<<endl;
+        // if(m1[A[i]]>=ceil((double)A.size()/2))return A[i];
+    }
+        for(auto iter=m1.begin();iter!=m1.end();iter++){
+            if(iter->second>floor((double)A.size()/3)){
+                ans.push_back(iter->first);
+            }
+        }
+    return ans;
+    }
+};
