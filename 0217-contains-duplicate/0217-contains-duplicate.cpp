fc2234228e69e5af165b3e7db4246fc90b36e496
snandkule <snandkule@gmail.com>
2023-06-23 21:42:23 +0530

    Time: 192 ms (22.50%), Space: 70.8 MB (27.12%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        set<int> s1;
+        for(int i=0;i<nums.size();i++){
+            if(s1.find(nums[i])!=s1.end())return true;
+            s1.insert(nums[i]);
+        }
+        return false;
+        
+    }
+};
