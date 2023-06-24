635984a191557e1c03fa5767b9667040093c864c
snandkule <snandkule@gmail.com>
2023-06-24 13:43:32 +0530

    Time: 243 ms (29.15%), Space: 98.5 MB (87.93%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -4,19 +4,14 @@ public:
         stack<int> s1;
         int n = temp.size();
         vector<int> ans(n,0);
-        
         s1.push(n-1);
         
         for(int i=n-2;i>=0;i--){
-            
             while(!s1.empty() && temp[s1.top()]<=temp[i]){
                 s1.pop();
             }
-            
             if(!s1.empty()){
-               
-                     ans[i] = s1.top() - i;
-  
+                ans[i] = s1.top() - i;
             }
              s1.push(i);
         }
