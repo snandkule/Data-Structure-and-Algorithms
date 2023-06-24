2b2e78a5bf19463d0484b83e1edf855bb73605d2
snandkule <snandkule@gmail.com>
2023-06-24 10:31:14 +0530

    Time: 17 ms (28.20%), Space: 7.2 MB (81.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end()); sort(t.begin(), t.end()); return s == t;
+//         if(s.length()!=t.length())return false;
+//         int m1[26];
+//         int m2[26];
+//          for(int i=0;i<26;i++){
+//             m1[i]=0;
+//              m2[i]=0;
+//         }
         
-        if(s.length()!=t.length())return false;
-        int m1[26];
-        int m2[26];
-         for(int i=0;i<26;i++){
-            m1[i]=0;
-             m2[i]=0;
-        }
+//         for(int i=0;i<s.length();i++){
+//             m1[s[i]-'a']++;
+//              m2[t[i]-'a']++;
+//         }
         
-        for(int i=0;i<s.length();i++){
-            m1[s[i]-'a']++;
-             m2[t[i]-'a']++;
-        }
-        
-         for(int i=0;i<26;i++){
-            if(m2[i] != m1[i])return false;
-        }
+//          for(int i=0;i<26;i++){
+//             if(m2[i] != m1[i])return false;
+//         }
    
-        return true;
+//         return true;
         
     }
 };
