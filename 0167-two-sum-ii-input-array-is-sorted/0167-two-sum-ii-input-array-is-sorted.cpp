3eb0d1aff4ce4e590bcb945b034bdfde6d73f784
snandkule <snandkule@gmail.com>
2023-06-28 00:56:43 +0530

    Time: 20 ms (19.21%), Space: 15.6 MB (73.55%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -8,11 +8,11 @@ public:
         vector<int> ans(2,1);
         while(i<j){
             
-            if((long long)numbers[i]+(long long)numbers[j]==(long long)target){
+            if((long long)numbers[i]+(long long)numbers[j]==target){
                 ans[0]+=i;
                 ans[1]+=j;
                 return ans;
-            }else if((long long)numbers[i]+(long long)numbers[j]>(long long)target){
+            }else if((long long)numbers[i]+(long long)numbers[j]>target){
                 j--;
             }else{
                 i++;
