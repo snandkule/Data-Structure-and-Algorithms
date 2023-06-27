361c1c5769cb59d20e240bf7a93985c7b13832a4
snandkule <snandkule@gmail.com>
2023-06-28 00:41:56 +0530

    Time: 8 ms (94.48%), Space: 10.8 MB (31.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,12 +5,8 @@ public:
      
         unordered_map<int,int> m1;
         vector<int> ans(2,0);
-        for(int i=0;i<nums.size();i++){
-            if(i==0){
-            
-                m1[nums[i]]=i;
-                continue;
-            }
+        m1[nums[0]]=0;
+        for(int i=1;i<nums.size();i++){
             if(m1.find(target-nums[i])!=m1.end()){
                 ans[0] = m1[target-nums[i]];
                 ans[1] = i;
