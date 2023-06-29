342fc519e81d1122f74a183476d70a8bb588a195
snandkule <snandkule@gmail.com>
2023-06-29 14:05:33 +0530

    Time: 383 ms (36.43%), Space: 34.7 MB (31.75%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -12,9 +12,10 @@ public:
         sort(nums.begin(),nums.end());
         set<vector<int>> s1;
    
-        for(int i=0;i<nums.size();i++){
-            
+        for(int i=0;i<nums.size()-2;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
             int j=i+1,k=nums.size()-1;
+            
             while(j<k){
                 if(nums[j]+nums[k] == -1*nums[i]){
                        vector<int> tmp(3,nums[i]);
