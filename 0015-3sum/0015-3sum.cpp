ec2961553750a17d67fa8bc23a7ea04e67557081
snandkule <snandkule@gmail.com>
2023-06-29 14:03:36 +0530

    Time: 2217 ms (5.00%), Space: 303.2 MB (6.61%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
+    void checkAndUpdateAns(vector<int> &tmp, set<vector<int>> &s1, vector<vector<int>> &ans){
+       if(s1.find(tmp)==s1.end()){
+            ans.push_back(tmp);
+           s1.insert(tmp);
+       }
+    }
     vector<vector<int>> threeSum(vector<int>& nums) {
       
     vector<vector<int>> ans;
-    
+        sort(nums.begin(),nums.end());
         set<vector<int>> s1;
-        unordered_map<int,int> s2;
-        s2[nums[0]]=1;
-        for(int i=1;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                
-                if(s2.find((nums[i]+nums[j])*-1)!=s2.end()){
-                     vector<int> tmp(3,nums[i]);
+   
+        for(int i=0;i<nums.size();i++){
+            
+            int j=i+1,k=nums.size()-1;
+            while(j<k){
+                if(nums[j]+nums[k] == -1*nums[i]){
+                       vector<int> tmp(3,nums[i]);
                        tmp[1]=nums[j];
-                       tmp[2]=(nums[i]+nums[j])*-1;
-                       if(tmp[0]>tmp[1]){
-                           int r = tmp[0];
-                           tmp[0]=tmp[1];
-                           tmp[1]=r;
-                       }
-                       if(tmp[0]>tmp[2]){
-                           int r = tmp[0];
-                           tmp[0]=tmp[2];
-                           tmp[2]=r;
-                       }
-                       if(tmp[1]>tmp[2]){
-                           int r = tmp[1];
-                           tmp[1]=tmp[2];
-                           tmp[2]=r;
-                       }
-                       if(s1.find(tmp)==s1.end()){
-                            ans.push_back(tmp);
-                           s1.insert(tmp);
-                       }
-                    
+                       tmp[2]=nums[k];
+                    checkAndUpdateAns(tmp,s1,ans);
+                    j++;
+                }else if(nums[j]+nums[k] > -1*nums[i]){
+                    k--;
+                }else{
+                    j++;
                 }
+                
             }
-            s2[nums[i]]=1;
         }
         return ans;
         
