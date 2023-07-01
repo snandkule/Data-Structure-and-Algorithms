aa6e20faad36e7947eb89253df7ba91ece8856c4
snandkule <snandkule@gmail.com>
2023-07-02 00:49:14 +0530

    Time: 4 ms (96.43%), Space: 6.9 MB (76.94%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -19,19 +19,21 @@ public:
         int start =0;
         int ans=0;
         int curr=0;
+        int maxfreq=0;
         while(curr<s.length()){
             
             arr[s[curr]-'A']++;
-            if(!checkValid(arr,k,start,curr)){
+            
+            maxfreq = max(maxfreq, arr[s[curr]-'A']);
+            bool isvalid = (curr-start+1-maxfreq<=k);
+            if(!isvalid){
                 arr[s[start]-'A']--;
-                ans = max(ans,curr-start);
                 start++;
             }
+             ans = max(ans,curr-start+1);
             curr++;
         }
-        if(checkValid(arr,k,start,curr-1)){
-             ans = max(ans,curr-start);
-        }
+    
         return ans;
     }
 };
