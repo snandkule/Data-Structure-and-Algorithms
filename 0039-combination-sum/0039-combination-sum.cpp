4e216bd994564123324ec0fcaf02eef0e72435d7
snandkule <snandkule@gmail.com>
2023-07-13 05:46:22 +0530

    Time: 23 ms (33.12%), Space: 15.6 MB (37.99%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -5,47 +5,27 @@ public:
 //     }
     
 
-set<vector<int>> s1;
-void getsum(vector<int> &A, vector<int> &arr, int i, long long s, int B){
-    if(i>=A.size()||s>=B){
-        if(s==B){
-         s1.insert(arr); 
-       
+    void combination(vector<int>& candidates, int target, vector<int> currComb, int currSum, int currIndex, vector<vector<int>>& ans){
+        if(currSum>target) return; //backtrack
+        if(currSum==target){
+            ans.push_back(currComb); //store the solution and backtrack
+            return;
         }
-        return;
-    }
-    getsum(A,arr,i+1,s,B);
-    arr.push_back(A[i]);
-    s+=A[i];
-    getsum(A,arr,i,s,B);
-    
-    // getsum(A,arr,i+1,s,B);
-    
-    arr.pop_back();
-}
-
-vector<vector<int> > combinationSum(vector<int> &A, int B) {
-    
-    sort(A.begin(),A.end());
-    vector<int> arr(0);
-    
-    getsum(A,arr,0,0,B);
-    
-    vector<vector<int>> ans(0);
-    ans.assign(s1.begin(),s1.end());
-    
-    sort(ans.begin(),ans.end(),[](const vector<int> &a1, const vector<int> &a2)->bool{
-        int i=0,j=0;
         
-        while(i<a1.size() && j<a2.size()){
-            if(a1[i]<a2[j])return true;
-            else if(a1[i]>a2[j])return false;
-            i++;j++;
+        for(int i=currIndex; i<candidates.size(); i++){ //try all possible options for the next level
+            currComb.push_back(candidates[i]); //put 1 option into the combination
+            currSum+=candidates[i];
+            combination(candidates, target, currComb, currSum, i, ans); //try with this combination, whether it gives a solution or not.
+            currComb.pop_back(); //when this option backtrack to here, remove this and go on to the next option.
+            currSum-=candidates[i];
         }
-        if(i>=a1.size())return true;
-        return false;
-    });
-    return ans;
-    
-}
+        
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> currComb;
+        combination(candidates, target, currComb, 0, 0, ans);
+        return ans;
+    }
 };
