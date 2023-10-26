71c1a06fed81c43777459144e0730655f6c3fcce
snandkule <snandkule@gmail.com>
2023-10-26 19:48:01 +0200

    Time: 0 ms (100.00%), Space: 40.4 MB (84.62%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,20 +1,20 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         
-        String ans="";
+        StringBuilder ans = new StringBuilder();
         int i=0,m=word1.length(),n=word2.length();
         while(i<m && i<n){
-            ans+=word1.charAt(i);
-            ans+=word2.charAt(i);
+            ans.append(word1.charAt(i));
+             ans.append(word2.charAt(i));
             i++;
         }
         if(i<m){
-            ans+=word1.substring(i);
+            ans.append(word1.substring(i));
         }
         if(i<n){
-            ans+=word2.substring(i);
+            ans.append(word2.substring(i));
         }
-        return ans;
+        return ans.toString();
         
     }
 }
