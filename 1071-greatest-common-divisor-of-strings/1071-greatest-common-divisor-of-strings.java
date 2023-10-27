eecaa2912f0657bec1d570951b4bb983dad59e36
snandkule <snandkule@gmail.com>
2023-10-27 16:10:47 +0200

    Time: 1 ms (83.15%), Space: 41.1 MB (63.57%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,32 +1,15 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        if(str1.length()<str2.length()){
-            String tmp = str2;
-            str2 = str1;
-            str1 = tmp;
-        }
-        int m = str1.length(),n = str2.length(), tmp=n;
-        while(tmp>0 && !(m%tmp==0 && n%tmp==0)){
-            tmp=tmp-1;
-        }
-        System.out.println("tmp=" + tmp);
-        StringBuilder ans = new StringBuilder();
-        if(tmp==0)return ans.toString();
+        if(!(str1+str2).equals(str2+str1))return "";
         
-        String subs = str2.substring(0,tmp);
-         System.out.println("subs=" + subs);
-        while(ans.length()<str1.length()){
-            ans.append(subs);
-        }
-         System.out.println("ans updated=" + ans);
-        if(!ans.toString().equals(str1))return "";
+        int m = str1.length(), n = str2.length();
         
-        ans.setLength(0);
-         while(ans.length()<str2.length()){
-            ans.append(subs);
-        }
-         System.out.println("ans updated 2=" + ans);
-        if(!ans.toString().equals(str2))return "";
-        return subs;          
+        int l = gcd(m,n);
+        return str2.substring(0,l);
+    }
+  
+    public static int gcd(int p, int q) {
+        if (q == 0) return p;
+        else return gcd(q, p % q);
     }
 }
