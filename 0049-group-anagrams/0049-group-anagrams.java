a31c8b2ee8e89563ae9fdd83927fe0f14a0d66cd
snandkule <snandkule@gmail.com>
2023-11-17 18:49:47 +0100

    Time: 8 ms (31.05%), Space: 46.8 MB (61.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,31 +1,43 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
-        List<char[]> l1 = new ArrayList<>();
-        boolean[] visited = new boolean[strs.length];
+        // List<char[]> l1 = new ArrayList<>();
+        // boolean[] visited = new boolean[strs.length];
+        // for(String s: strs){
+        //     char[] schararr = s.toCharArray();
+        //     Arrays.sort(schararr);
+        //     l1.add(schararr);
+        // }
+        // List<List<String>> ans = new ArrayList<>();
+        // for(int i=0; i< strs.length;i++){
+        //     if(visited[i]){
+        //         continue;
+        //     }
+        //     List<String> list1 = new ArrayList<>();
+        //     list1.add(strs[i]);
+        //     visited[i]=true;
+        //     for(int j=i+1;j<strs.length;j++){
+        //         if(visited[j]==false){
+        //             if(Arrays.equals(l1.get(i),l1.get(j))){
+        //                 visited[j]=true;
+        //                 list1.add(strs[j]);
+        //             }
+        //         }
+        //     }
+        //     ans.add(list1);
+        // }
+        // return ans;
+        
+        Map<String, List<String>> m1 = new HashMap<>();
+
         for(String s: strs){
-            char[] schararr = s.toCharArray();
-            Arrays.sort(schararr);
-            l1.add(schararr);
-        }
-        List<List<String>> ans = new ArrayList<>();
-        for(int i=0; i< strs.length;i++){
-            if(visited[i]){
-                continue;
+            char[] schar = s.toCharArray();
+            Arrays.sort(schar);
+            if(!m1.containsKey(String.valueOf(schar))){
+                m1.put(String.valueOf(schar), new ArrayList<>());
             }
-            List<String> list1 = new ArrayList<>();
-            list1.add(strs[i]);
-            visited[i]=true;
-            for(int j=i+1;j<strs.length;j++){
-                if(visited[j]==false){
-                    if(Arrays.equals(l1.get(i),l1.get(j))){
-                        visited[j]=true;
-                        list1.add(strs[j]);
-                    }
-                }
-            }
-            ans.add(list1);
+            m1.get(String.valueOf(schar)).add(s);
         }
-        return ans;
+        return new ArrayList<>(m1.values());
     }
 }
