3b9901bc8c1ad4a9b23e57097be082931e5037fc
snandkule <snandkule@gmail.com>
2023-11-17 18:17:42 +0100

    Time: 5 ms (54.80%), Space: 42.6 MB (70.88%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arrs = new int[26];
+        int[] arrt = new int[26];
+        if(s.length()!=t.length())return false;
+
+        for(int i=0;i<s.length();i++){
+            arrs[s.charAt(i)-'a']++;
+            arrt[t.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(arrs[i]!=arrt[i])return false;
+        }
+        return true;
+
+
+    }
+}
