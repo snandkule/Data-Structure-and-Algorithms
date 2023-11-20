b212a440336c6fda1374ab83bbd164dafc24b545
snandkule <snandkule@gmail.com>
2023-11-20 16:49:23 +0100

    Time: 8 ms (34.83%), Space: 7.6 MB (62.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    bool isPalindrome(string A) {
-            string B;
-    for(int j = 0; j<A.length();j++){
-        if((A[j]>=65 && A[j]<=90)||(A[j]>=97 && A[j]<=122) || (A[j]>=48 && A[j]<=57))B.push_back(A[j]);
-    }
-    int j=0;
-    int i= B.length()-1;
-    while(i>=j){
-            if(tolower(B[i])!=tolower(B[j]))return 0;
-            i--;j++;
-    }
-    
-  return 1;
+    bool isPalindrome(string s) {
+        
+        int i=0,j = s.length()-1;
+        for(int k=0;k<s.length();k++){
+              if(s[k]>='A' && s[k]<='Z'){
+                  s[k] = 'a' + (s[k]-'A');
+              }
+        }
+        while(i<=j){
+            if(!((s[i]>='a' && s[i]<='z')||(s[i]>='0' && s[i]<='9'))){
+                i++;
+                continue;
+            }
+            if(!((s[j]>='a' && s[j]<='z')||(s[j]>='0' && s[j]<='9'))){
+                j--;
+                continue;
+            }
+            // cout<<s[i]<<","<<s[j]<<endl;
+            if(s[i]==s[j]){
+                i++;
+                j--;
+                continue;
+            }
+            return false;
+        }
+        return true;
         
     }
 };
