2a07112569efe7b915d89c5036573168ca4539f3
snandkule <snandkule@gmail.com>
2023-11-20 15:28:39 +0100

    Time: 16 ms (73.14%), Space: 24.3 MB (71.92%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> prefix(nums.size());
-        vector<int> postfix(nums.size());
-        vector<int> ans(nums.size(),1);
-        prefix[0] = nums[0];
-        postfix[nums.size()-1] = nums[nums.size()-1];
-        
-        for(int i=1;i<nums.size();i++){
-            prefix[i] = nums[i]*prefix[i-1];
-            postfix[nums.size()-i-1] = nums[nums.size()-i-1]*postfix[nums.size()-i];
-            // cout<<"prefix[i] ="<<prefix[i]<<", postfix[nums.size()-i-1] ="<<postfix[nums.size()-i-1]<<endl;
-            ans[i] *= prefix[i-1];
-            ans[nums.size()-i-1] *= postfix[nums.size()-i];
-            
+        int n = nums.size();
+        vector<int> output(n);
+        output[0] = 1;
+        for(int i=1; i<n; i++){
+            output[i] = output[i-1] * nums[i-1];
         }
-        ans[0]= postfix[1];
-        ans[nums.size()-1] = prefix[nums.size()-2];
-        // for(int i=1;i<nums.size()-1;i++){
-        //     ans[i] = postfix[i+1]*prefix[i-1];
-        // }
-        return ans;
+        int right = 1;
+        for(int i=n-1; i>=0; i--){
+            output[i] *= right;
+            right *= nums[i];
+        }
+        return output;
         
         
     }
