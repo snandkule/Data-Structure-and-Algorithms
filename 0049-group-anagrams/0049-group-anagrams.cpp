059b15a6367eb284fe95d68eb55a8724475d7433
snandkule <snandkule@gmail.com>
2023-11-20 11:35:32 +0100

    Time: 35 ms (35.58%), Space: 20.3 MB (69.20%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -8,13 +8,9 @@ public:
         for(int i=0;i<strs.size();i++){
             string s = strs[i];
             sort(s.begin(),s.end());
-            if(m1.find(s)==m1.end()){
-                vector<string> tmp;
-                tmp.push_back(strs[i]);
-                m1[s] = tmp;
-            }else{
+            
                 m1[s].push_back(strs[i]);
-            }
+            
         }
         for (const auto& pair : m1) {
             ans.push_back(pair.second);
