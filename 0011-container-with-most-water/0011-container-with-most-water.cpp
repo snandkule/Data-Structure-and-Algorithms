dc2634ca5b1449db709b7cea636ebc90836b8c0e
snandkule <snandkule@gmail.com>
2023-11-22 18:37:17 +0100

    Time: 75 ms (30.43%), Space: 59.4 MB (48.86%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -7,12 +7,16 @@ public:
         int i=0,j=height.size()-1;
         
         while(i<j){
-            ans = max(ans,(j-i)*min(height[i],height[j]));
-            if(height[i]<height[j]){
-                i++;
-            }else{
-                j--;
-            }
+            int h =min(height[i],height[j]);
+            ans = max(ans,(j-i)*h);
+            
+            while (height[i] <= h && i < j) i++;
+            while (height[j] <= h && i < j) j--;
+            // if(height[i]<height[j]){
+            //     i++;
+            // }else{
+            //     j--;
+            // }
         }
         return ans;
         
