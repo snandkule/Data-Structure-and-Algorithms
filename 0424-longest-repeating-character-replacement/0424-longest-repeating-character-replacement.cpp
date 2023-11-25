e6aa23c014daff33ad3f5c300ab6a09212d5ff42
snandkule <snandkule@gmail.com>
2023-11-25 23:21:31 +0100

    Time: 107 ms (5.04%), Space: 7.3 MB (65.99%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    unsigned long  getMax(vector<unsigned long>& arr){
-        unsigned long cnt=0;
-        for(unsigned long i=0;i<26;i++){
-            if(arr[cnt]<arr[i]){
-                cnt=i;
-            }
-        }
-        return cnt;
-    }
+    // unsigned long  getMax(vector<unsigned long>& arr){
+    //     unsigned long cnt=0;
+    //     for(unsigned long i=0;i<26;i++){
+    //         if(arr[cnt]<arr[i]){
+    //             cnt=i;
+    //         }
+    //     }
+    //     return cnt;
+    // }
     int characterReplacement(string s, int k) {
         
-        unsigned long ans=0;
-        vector<unsigned long> cntarr(26,0);
-        unsigned long start=0;
+        int ans=0;
+        vector<int> cntarr(26,0);
+        int maxf=0;
         
-        for(unsigned long i=0;i<s.length();i++){
-            
-            
+        for(int i=0;i<s.length();i++){
             cntarr[s[i]-'A']++;
-            unsigned long maxcnt = getMax(cntarr);  
-            
-            while(cntarr[maxcnt]+k < i-start+1){
-                cntarr[s[start]-'A']--;
-                if(maxcnt==s[start]-'A')
-                    maxcnt = getMax(cntarr);
-                start++;
+            maxf = max(maxf, cntarr[s[i]-'A']); 
+            cout<<"ans ="<<ans<<", maxf ="<<maxf<<endl;
+            if(ans-maxf<k){
+                ans++;
+            }else{
+                cntarr[s[i-ans]-'A']--;
             }
-                // if(maxcnt+k >= i-start+1){
-                    ans = max(ans, i-start+1);
-                // }
+           
             
         }
        
