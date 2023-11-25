b8690d125f0e2e7d014ffbbc41aaf55bb960c912
snandkule <snandkule@gmail.com>
2023-11-25 13:06:16 +0100

    Time: 0 ms (100.00%), Space: 8.6 MB (74.52%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int start=0;
-        map<char,bool> m1;
+//         int start=0;
+//         map<char,bool> m1;
         
-        unsigned long ans=0;
+//         unsigned long ans=0;
         
-        for(unsigned long i=0;i<s.length();i++){
-            if(m1[s[i]]==false){
-                m1[s[i]] = true;
-            }else{
-                ans = max(ans,i-start);
-                while(m1[s[i]] && start<i){
-                    m1[s[start]] = false;
-                    start++;
-                }
-                m1[s[i]] = true;
-            }
+//         for(unsigned long i=0;i<s.length();i++){
+//             if(m1[s[i]]==false){
+//                 m1[s[i]] = true;
+//             }else{
+//                 ans = max(ans,i-start);
+//                 while(m1[s[i]] && start<i){
+//                     m1[s[start]] = false;
+//                     start++;
+//                 }
+//                 m1[s[i]] = true;
+//             }
+//         }
+//         ans = max(ans, s.length()-start);
+//         return ans;
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
         }
-        ans = max(ans, s.length()-start);
-        return ans;
+        return maxLen;
         
     }
 };
