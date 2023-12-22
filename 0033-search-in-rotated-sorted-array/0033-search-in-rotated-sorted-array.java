b579ad899cb3e9cf870ca57fd08260a5ac3468b7
snandkule <snandkule@gmail.com>
2023-12-23 00:01:41 +0100

    Time: 0 ms (100.00%), Space: 41.9 MB (5.09%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,34 +1,56 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        
-        int start=0,end=nums.length-1,mid=0;
-        
-        while(start<=end){
-            mid=(start+end)/2;
-            // System.out.println(start+", "+mid+", "+end);
-            if(nums[mid]==target){
+         int start = 0;
+        int end = nums.length - 1;
+        while (start <= end){
+            int mid = (start + end) / 2;
+            if (nums[mid] == target)
                 return mid;
+        
+            if (nums[start] <= nums[mid]){
+                 if (target < nums[mid] && target >= nums[start]) 
+                    end = mid - 1;
+                 else
+                    start = mid + 1;
+            } 
+        
+            if (nums[mid] <= nums[end]){
+                if (target > nums[mid] && target <= nums[end])
+                    start = mid + 1;
+                 else
+                    end = mid - 1;
             }
-            if(nums[start]<nums[end]){
-                if(nums[mid]<target){
-                    start = mid+1;
-                }else{
-                    end = mid-1;
-                }
-            }else{
-                if((nums[start]<=target && target<=nums[mid]  && nums[mid]>=nums[end])||
-                   (nums[start]<=target && target>=nums[mid] && nums[mid]<=nums[end])||
-                  (nums[start]>=target && target<=nums[mid] && nums[mid]<=nums[end])){
-                    end = mid-1;
-                }else{
-                    start= mid+1;
-                }
-                
-            }
-            
         }
         return -1;
+        
+//         int start=0,end=nums.length-1,mid=0;
+        
+//         while(start<=end){
+//             mid=(start+end)/2;
+//             // System.out.println(start+", "+mid+", "+end);
+//             if(nums[mid]==target){
+//                 return mid;
+//             }
+//             if(nums[start]<nums[end]){
+//                 if(nums[mid]<target){
+//                     start = mid+1;
+//                 }else{
+//                     end = mid-1;
+//                 }
+//             }else{
+//                 if((nums[start]<=target && target<=nums[mid]  && nums[mid]>=nums[end])||
+//                    (nums[start]<=target && target>=nums[mid] && nums[mid]<=nums[end])||
+//                   (nums[start]>=target && target<=nums[mid] && nums[mid]<=nums[end])){
+//                     end = mid-1;
+//                 }else{
+//                     start= mid+1;
+//                 }
+                
+//             }
+            
+//         }
+//         return -1;
     }
 }
 
