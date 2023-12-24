c38d84ea853029b2ad7b88bf1a4243256bc31b10
snandkule <snandkule@gmail.com>
2023-12-24 14:33:39 +0100

    Time: 0 ms (100.00%), Space: 40.8 MB (6.17%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -18,9 +18,8 @@ class Solution {
         if(p==null && q==null){
             return true;
         }
-        if(p==null || q==null)return false;
-        if(p.val!=q.val)return false;
-        
+        if(p==null || q==null || p.val!=q.val)return false;
+      
         if(!isSameTree(p.left,q.left) || !isSameTree(p.right,q.right)){
             return false;
         }
