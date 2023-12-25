1a74d43d4cf86ed9fae9d4ed0c69a3b9ec04dba0
snandkule <snandkule@gmail.com>
2023-12-25 13:58:27 +0100

    Time: 10 ms (6.41%), Space: 44.8 MB (6.27%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,20 +2,17 @@ class Solution {
     Set<List<Integer>> ans = new HashSet<>();
     
     void checkCombination(int[] candidates, int index, List<Integer> arr, int currSum, int target){
-        if(currSum>target){
-            return;
-        }
-        if(index>=candidates.length && currSum!=target){
+      
+        if((currSum>target)||(index>=candidates.length && currSum!=target)){
             return;
         }
         if(currSum==target){
             ans.add(new ArrayList<>(arr));
             return;
         }
-        // List<Integer> arr1 = new ArrayList<>(arr);
+
         checkCombination(candidates, index+1, arr,currSum,target);
         currSum+=candidates[index];
-        // List<Integer> arr2 = new ArrayList<>(arr);
         arr.add(candidates[index]);
         checkCombination(candidates, index, arr,currSum,target);
         checkCombination(candidates, index+1, arr,currSum,target);
