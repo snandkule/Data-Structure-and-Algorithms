e13bd7e6084bf4ab7eb38217320c4508e76c57ef
snandkule <snandkule@gmail.com>
2023-12-26 16:56:30 +0100

    Time: 28 ms (26.69%), Space: 42.8 MB (5.97%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,20 +19,21 @@ class Node {
 */
 
 class Solution {
-    Map<Integer, Node> m1 = new HashMap<>();
+    // Map<Integer, Node> m1 = new HashMap<>();
     boolean[] visited = new boolean[101];
+    Node[] m1 = new Node[101];
     public Node cloneGraph(Node node) {
         if(node==null)return null;
-        if(visited[node.val])return m1.get(node.val);
+        if(visited[node.val])return m1[node.val];
         
         Node curr = new Node(node.val);
         visited[node.val] = true;
-         m1.put(curr.val, curr);
+         m1[curr.val] =curr;
         for(int i=0;i< node.neighbors.size();i++){
             if(!visited[node.neighbors.get(i).val]){
-                m1.put(node.neighbors.get(i).val, cloneGraph(node.neighbors.get(i)));
+                m1[node.neighbors.get(i).val] =cloneGraph(node.neighbors.get(i));
             }
-            curr.neighbors.add(m1.get(node.neighbors.get(i).val));
+            curr.neighbors.add(m1[node.neighbors.get(i).val]);
         }
         return curr;
         
