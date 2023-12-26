4d0364a077b015305963588ade4500b58b078e7f
snandkule <snandkule@gmail.com>
2023-12-26 15:12:00 +0100

    Time: 0 ms (100.00%), Space: 43.6 MB (87.28%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -17,8 +17,9 @@ class Solution {
     
     public boolean check(TreeNode root, long high, long low){
         if(root==null)return true;
-        if(root.val<=low || root.val>=high)return false;
-        if(!check(root.left, root.val,low) || !check(root.right, high,root.val))return false;
+        if((root.val<=low || root.val>=high) || 
+           !check(root.left, root.val,low) || 
+           !check(root.right, high,root.val))return false;
         
         return true;
         
