947ee9dbe8ef22520e3d7b7efa58c5293544fc41
snandkule <snandkule@gmail.com>
2023-12-27 13:18:36 +0100

    Time: 5 ms (67.48%), Space: 44.7 MB (21.45%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,17 +1,83 @@
 class Solution {
     public boolean canFinish(int n, int[][] prerequisites) {
-         ArrayList<Integer>[] G = new ArrayList[n];
-        int[] degree = new int[n];
-        ArrayList<Integer> bfs = new ArrayList();
-        for (int i = 0; i < n; ++i) G[i] = new ArrayList<Integer>();
-        for (int[] e : prerequisites) {
-            G[e[1]].add(e[0]);
-            degree[e[0]]++;
+        
+        List<Integer>[] G = new ArrayList[n];
+        int[] inDegree = new int[n];
+        for(int i=0;i<n;i++){
+            List<Integer> t = new ArrayList<>();
+            G[i]=t;
+            inDegree[i]=0;
         }
-        for (int i = 0; i < n; ++i) if (degree[i] == 0) bfs.add(i);
-        for (int i = 0; i < bfs.size(); ++i)
-            for (int j: G[bfs.get(i)])
-                if (--degree[j] == 0) bfs.add(j);
-        return bfs.size() == n;
+        
+        for(int i=0;i<prerequisites.length;i++){
+            G[prerequisites[i][1]].add(prerequisites[i][0]);
+            inDegree[prerequisites[i][0]]++;
+        }
+        
+        List<Integer> bfs = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(inDegree[i]==0)bfs.add(i);
+        }
+        
+        for(int i=0;i<bfs.size();i++){
+            
+            for(int j=0;j<G[bfs.get(i)].size();j++){
+                if(--inDegree[G[bfs.get(i)].get(j)]==0){
+                    bfs.add(G[bfs.get(i)].get(j));
+                }
+            }
+        }
+         
+        return bfs.size()==n;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ArrayList<Integer>[] G = new ArrayList[n];
+//         int[] degree = new int[n];
+//         ArrayList<Integer> bfs = new ArrayList();
+//         for (int i = 0; i < n; ++i) G[i] = new ArrayList<Integer>();
+//         for (int[] e : prerequisites) {
+//             G[e[1]].add(e[0]);
+//             degree[e[0]]++;
+//         }
+//         for (int i = 0; i < n; ++i) if (degree[i] == 0) bfs.add(i);
+//         for (int i = 0; i < bfs.size(); ++i)
+//             for (int j: G[bfs.get(i)])
+//                 if (--degree[j] == 0) bfs.add(j);
+//         return bfs.size() == n;
