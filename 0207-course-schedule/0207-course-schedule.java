d28af744e3f7650cdb496155797de14df9cf838c
snandkule <snandkule@gmail.com>
2023-12-27 13:01:08 +0100

    Time: 4 ms (81.60%), Space: 44.4 MB (40.14%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,50 +1,17 @@
 class Solution {
-    
-    public int get_visited_cnt( boolean[] visited){
-        int cnt=0;
-        for(int i=0;i<visited.length;i++){
-            if(visited[i])cnt++;
+    public boolean canFinish(int n, int[][] prerequisites) {
+         ArrayList<Integer>[] G = new ArrayList[n];
+        int[] degree = new int[n];
+        ArrayList<Integer> bfs = new ArrayList();
+        for (int i = 0; i < n; ++i) G[i] = new ArrayList<Integer>();
+        for (int[] e : prerequisites) {
+            G[e[1]].add(e[0]);
+            degree[e[0]]++;
         }
-        return cnt;
-    }
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
-        List<Integer>[] arr = new ArrayList[numCourses];
-        
-        for(int i=0;i<numCourses;i++){
-            List<Integer> t = new ArrayList<>();
-            arr[i] =t;
-        }
-        for(int i=0;i<prerequisites.length;i++){
-            arr[prerequisites[i][0]].add(prerequisites[i][1]);
-        }
-        
-        boolean[] visited = new boolean[numCourses];
-        
-        while(get_visited_cnt(visited)!=numCourses){
-            
-            List<Integer> curr = new ArrayList<>();
-            
-            for(int i=0;i<numCourses;i++){
-            
-                if(arr[i].size()==0 && visited[i]==false){
-                    curr.add(i);
-                    visited[i]= true;
-                }
-            }
-            
-            if(curr.size()==0){
-                return false;
-            }
-            
-            for(int i=0;i<curr.size();i++){
-                 for(int j=0;j<numCourses;j++){
-                     arr[j].remove(curr.get(i));
-                 }
-            }
-            
-        }
-        return true;
-        
+        for (int i = 0; i < n; ++i) if (degree[i] == 0) bfs.add(i);
+        for (int i = 0; i < bfs.size(); ++i)
+            for (int j: G[bfs.get(i)])
+                if (--degree[j] == 0) bfs.add(j);
+        return bfs.size() == n;
     }
 }
