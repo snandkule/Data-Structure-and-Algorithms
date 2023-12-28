33c1791e60fdca0bba52a304ba0021c764d69f10
snandkule <snandkule@gmail.com>
2023-12-28 15:01:03 +0100

    Time: 1 ms (78.84%), Space: 41.6 MB (48.63%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,32 +1,36 @@
 class Solution {
     public int numDecodings(String s) {
         
-        int[] arr = new int[s.length()+1];
+        // int[] arr = new int[s.length()+1];
+        int prev=0, prev2=0;
         for(int i=0;i<s.length();i++){
-            arr[i]=0;
+            int curr=0;
+            // arr[i]=0;
             if(s.charAt(i)-'1'>=0 && s.charAt(i)-'1'<=8){
                 if(i>0)
-                arr[i]+=arr[i-1];
+                curr+=prev;
                 else
-                    arr[i]=1;
+                    curr=1;
             }
             if(i>0){
                 if(s.charAt(i-1)=='2'){
                     if(s.charAt(i)>='0' && s.charAt(i)<='6'){
                         if(i>1)
-                            arr[i]+=arr[i-2];
+                            curr+=prev2;
                         else
-                            arr[i]+=1;
+                            curr+=1;
                     }
                 }else if(s.charAt(i-1)=='1'){
                     if(i>1)
-                            arr[i]+=arr[i-2];
+                            curr+=prev2;
                         else
-                            arr[i]+=1;
+                            curr+=1;
                 }
             }
+            prev2 = prev;
+            prev = curr;
         }
-        return arr[s.length()-1];
+        return prev;
         
         
         
