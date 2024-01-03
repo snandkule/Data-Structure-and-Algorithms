6acf341fe660eab47652707dabb3043a7fb3fa48
snandkule <snandkule@gmail.com>
2024-01-03 17:59:22 +0100

    Time: 1 ms (99.98%), Space: 56.6 MB (95.35%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,20 +1,30 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        if(nums.length==1)return nums[0];
-        int ans=nums[0];
-        int curr = nums[0];
+//         if(nums.length==1)return nums[0];
+//         int ans=nums[0];
+//         int curr = nums[0];
         
-        for(int i=1;i<nums.length;i++){
-            // System.out.println(i+", curr="+curr);
-            if( curr+nums[i]<nums[i]){
-                ans = Math.max(ans,curr);
-                curr=nums[i];
-            }else{
-                curr+=nums[i];
-            }
-            ans = Math.max(ans,curr);
+//         for(int i=1;i<nums.length;i++){
+//             // System.out.println(i+", curr="+curr);
+//             if( curr+nums[i]<nums[i]){
+//                 ans = Math.max(ans,curr);
+//                 curr=nums[i];
+//             }else{
+//                 curr+=nums[i];
+//             }
+//             ans = Math.max(ans,curr);
+//         }
+//         return ans;
+         int n = nums.length;
+        int max = Integer.MIN_VALUE, sum = 0;
+        
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            max = Math.max(sum,max);
+            
+            if(sum<0) sum = 0;
         }
-        // ans = Math.max(ans,curr);
-        return ans;
+        
+        return max;
     }
 }
