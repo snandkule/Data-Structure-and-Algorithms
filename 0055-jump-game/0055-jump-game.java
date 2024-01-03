4d1f6031f66057fa50dcc8087473d38846f0b225
snandkule <snandkule@gmail.com>
2024-01-03 18:12:04 +0100

    Time: 3 ms (49.19%), Space: 45.7 MB (12.30%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -6,8 +6,7 @@ class Solution {
         
         for(int i=1;i<nums.length;i++){
             if(n-1<0)return false;
-            n--;
-            n = Math.max(n,nums[i]);
+            n = Math.max(n-1,nums[i]);
         }
         return true;
         
