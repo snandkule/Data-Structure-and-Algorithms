5d05df4303d3266e1f1a960bf16ec2a7f74522a2
snandkule <snandkule@gmail.com>
2024-01-25 08:40:58 +0100

    Time: 63 ms (91.05%), Space: 59.7 MB (27.78%) - LeetHub

--- 2895-minimum-processing-time/2895-minimum-processing-time.java
+++ 2895-minimum-processing-time/2895-minimum-processing-time.java
@@ -5,10 +5,9 @@ class Solution {
         int ans=processorTime.get(0)+tasks.get(0);
         int n=tasks.size()-1;
         for(int i=0;i<processorTime.size();i++){
-            for(int j=0;j<4;j++){
+            
                 ans = Math.max(ans,processorTime.get(i)+tasks.get(n) );
-                n--;
-            }
+              n-=4;
         }
         return ans;
         
