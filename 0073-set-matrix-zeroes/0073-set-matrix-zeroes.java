d3d04ec194c1811b3d1cccca398424fe6585d18d
snandkule <snandkule@gmail.com>
2024-01-28 14:45:21 +0100

    Time: 11 ms (5.22%), Space: 45.4 MB (48.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,71 +1,60 @@
 class Solution {
     
     void setM(int[][] matrix, int r, int c){
-        
-        for(int j=0;j<=c;j++){
-            matrix[r][j]=0;
-        }
-        for(int j=c+1;j<matrix[0].length;j++){
-            // if(matrix[r][j]==0){
-            //     matrix[r][j]=-1;
-            // }else{
+        for(int j=0;j<matrix[0].length;j++){
+            if(matrix[r][j]==0){
                 matrix[r][j]=0;
-            // }
+            }else{
+                matrix[r][j]=-10000;
+            }
         }
-        for(int i=0;i<=r;i++){
-            matrix[i][c]=0;
-        }
-        for(int i=r+1;i<matrix.length;i++){
-            // if(matrix[i][c]==0){
-            //     matrix[i][c]=-1;
-            // }else{
+       
+        for(int i=0;i<matrix.length;i++){
+            if(matrix[i][c]==0){
                 matrix[i][c]=0;
-            // }
+            }else{
+                matrix[i][c]=-10000;
+            }
         }
+        matrix[r][c]=-10000;
+    }
+    boolean isRowSet(int[][] matrix, int r){
+        for(int j=0;j<matrix[0].length;j++){
+            if(matrix[r][j]!=-10000 && matrix[r][j]!=0)return false;
+        }
+        return true;
+    }
+    boolean isColSet(int[][] matrix, int c){
+        for(int i=0;i<matrix.length;i++){
+            if(matrix[i][c]!=-10000 && matrix[i][c]!=0)return false;
+        }
+        return true;
     }
     public void setZeroes(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
-//         for(int i=0;i<m;i++){
-//             int isz=0;
-//             for(int j=0;j<n;j++){
-                
-//                 if(isz==0 && matrix[i][j]==0){
-//                     isz=1;
-//                     setM(matrix,i,j);
-//                 }
-//                 if(isz==1 && matrix[i][j]==-1){
-//                     setM(matrix,i,j);
-//                 }
-//             }
-//         }
-        int[] rows = new int[m];
-        int[] cols = new int[n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){          
+                if(matrix[i][j]==0){
+                    setM(matrix,i,j);
+                    matrix[i][j]=-10000;
+                } 
+            }
+        }  
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    rows[i]=1;
-                    cols[j]=1;
+                if(isRowSet(matrix,i)){
+                    for(int k=0;k<n;k++){
+                        matrix[i][k]=0;
+                    }
+                }
+                if(isColSet(matrix,j)){
+                    for(int k=0;k<m;k++){
+                        matrix[k][j]=0;
+                    }
                 }
             }
         }
-        
-        for(int i=0;i<m;i++){
-            if(rows[i]==1){
-                for(int j=0;j<n;j++){
-                    matrix[i][j]=0;
-                }
-            }
-        }
-        
-        for(int j=0;j<n;j++){
-            if(cols[j]==1){
-                for(int i=0;i<m;i++){
-                    matrix[i][j]=0;
-                }
-            }
-        }
-        
-        
-        
     }
+    
 }
